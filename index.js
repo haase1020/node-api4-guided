ead67131a81b381f518b32624f96ff2e3fa43a42
Mandi Haase <haase1020@gmail.com>
2020-03-12 13:42:14 -0400

    update dotenv

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+require('dotenv').config();//read from a .env file located at the root of the project
+//and merge the key/value pairs into process.env
+
 const server = require('./api/server.js');
 
 
